1392ef4fcd0c68eed51622cc58b649a9c8bbcd32
Erni Durdevic <erni.durdevic@quby.com>
2020-06-25 15:47:00 +0200

    Better docstring for SparkActiveSession

--- scala/src/main/scala/com/quby/demo/databricks/util/SparkActiveSession.scala
+++ scala/src/main/scala/com/quby/demo/databricks/util/SparkActiveSession.scala
@@ -4,7 +4,15 @@ import org.apache.spark.sql.SparkSession
 import org.slf4j.{Logger, LoggerFactory}
 
 /**
-  * Extend this trait for having quick access to current SparkSession and logger.
+  * Extend this trait for having quick access to current SparkSession, logger and sets the current
+  * timezone on UTC (See FAQ on RunOnUtc trait).
+  * All transformation classes inherit SparkActiveSession for convenience.
+  *
+  * FAQ: Why do we set current timezone on UTC for all transformations?
+  *      --> Spark methods displays Timestamp columns always with Local System timezone.
+  *          Keeping the timezone aligned to UTC makes it easier to write unit tests and 
+  *          check the correctness of time values regardless of the timezone of the developer's 
+  *          machine.
   */
 trait SparkActiveSession extends RunOnUTC {
   val spark:  SparkSession = SparkSession.builder().getOrCreate()
